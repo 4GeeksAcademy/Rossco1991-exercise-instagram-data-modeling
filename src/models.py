bc966dccc2413f6b7eac77bb70b6f0f1fcd664a9
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-08-03 00:04:42 +0000

    Working on relationships with tables

--- src/models.py
+++ src/models.py
@@ -7,12 +7,14 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
 
 class Address(Base):
     __tablename__ = 'address'
@@ -28,7 +30,40 @@ class Address(Base):
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+class Posts(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer, ForeignKey('user.id'))
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(256), unique=True, nullable=False)
+    password = Column(String(256), unique=True,  nullable=False)
+    first_name = Column(String(256), unique=False, nullable=False)
+    last_name = Column(String(256), unique=False, nullable=False)
+    email = Column(String(256), unique=True, nullable=False)
+    posts = Column(Integer, ForeignKey('posts.id'))
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(256))
+    url = Column(String(256))
+    post = Column(Integer, ForeignKey('posts.id'))
+
+
+# Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
